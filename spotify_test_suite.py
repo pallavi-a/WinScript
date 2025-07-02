fbd0ee05649d70fa13e5e05a059ba757096e6c18
devodutt98 <devodutt98@gmail.com>
2025-07-02 11:25:56 -0700

    Update Spotify test suite

--- spotify_test_suite.py
+++ spotify_test_suite.py
@@ -2,7 +2,7 @@ from pywinauto.application import Application
 from pywinauto.keyboard import send_keys
 import pytest, time, os
 
-#comment added new update 3
+#comment added new update 4
 
 SPOTIFY_PATH = os.path.join(os.environ['APPDATA'], 'Local', 'Microsoft', 'WindowsApps', 'Spotify.exe')
 
